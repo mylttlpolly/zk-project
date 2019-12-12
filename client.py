eb4ffe80e8f3761789a785a2377e038a2c27c700
Roman Detinin <detininroman@gmail.com>
2019-12-12 12:03:07 +0300

    initial commit

---
+++ client.py
@@ -0,0 +1,38 @@
+import configparser
+import socket
+
+from Crypto.Hash import MD5
+from Crypto.PublicKey import RSA
+
+
+def main(port=8080):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        with open('mykey.pem', 'r') as f:
+            key = RSA.importKey(f.read())
+
+        pub = RSA.generate(2048).publickey()
+
+        with open('cl1key.pem', 'wb') as f:
+            f.write(pub.exportKey('PEM'))
+
+        sock.connect((socket.gethostbyname('localhost'), port))
+
+        d = sock.recv(4096)
+        e = int.from_bytes(d, 'big')
+
+        h = MD5.new(b'test message')
+        g = h.digest()
+        x = key.encrypt((g * e), 1)
+
+        sock.sendall((x[0]))
+
+        print('Message sent successfully')
+
+        reply = sock.recv(4096)
+        print(reply.decode())
+
+
+if __name__ == '__main__':
+    parser = configparser.ConfigParser()
+    parser.read('config.ini')
+    main(int(parser['DEFAULT']['port']))
---
+++ server.py
@@ -0,0 +1,47 @@
+import socket
+from random import randint
+import configparser
+
+from Crypto.PublicKey import RSA
+
+
+def main(port=8080):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        print('Socket created')
+
+        key = RSA.generate(2048)
+        pub = key.publickey()
+
+        with open('mykey.pem', 'wb') as f:
+            f.write(pub.exportKey('PEM'))
+
+        sock.bind(('localhost', port))
+        sock.listen()
+
+        nons = {k: randint(0, 9) for k in (0, 1)}
+
+        while True:
+            answer = {k: None for k in (0, 1)}
+            conn = {k: None for k in (0, 1)}
+
+            with open('cl1key.pem', 'r') as f:
+                cl1key = RSA.importKey(f.read())
+
+            for i in answer:
+                conn[i], addr = sock.accept()
+                conn[i].send(bytes([nons[i]]))
+                data = (conn[i].recv(1024))
+                print(data)
+                print('Connected with ' + addr[0] + ':' + str(addr[1]))
+                answer[i] = key.decrypt(data)
+
+            response = b'equal' if answer[0] * nons[1] == answer[1] * nons[0] else b'not equal'
+            for i in answer:
+                conn[i].send(response)
+            print('END')
+
+
+if __name__ == '__main__':
+    parser = configparser.ConfigParser()
+    parser.read('config.ini')
+    main(int(parser['DEFAULT']['port']))
